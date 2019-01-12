3d00085837583383828d0e2813a35faacc1708e7
unknown <marlonspr@hotmail.com>
2019-01-12 22:20:28 +1300

    My learning process files

---
+++ DOM Manipulation/index.js
@@ -0,0 +1,46 @@
+let form = document.querySelector("#addForm");
+let listItems = document.querySelector("#items");
+let filter = document.querySelector("#filter");
+
+form.addEventListener("submit", addElement);
+listItems.addEventListener("click", removeElement);
+filter.addEventListener("keyup", filterElement);
+
+function addElement(e) {
+    e.preventDefault();
+
+    let formInput = document.querySelector("#addForm input[type='text']").value;
+
+listItems.innerHTML += `<li class="list-group-item">${formInput}<button class="btn btn-danger btn-sm float-right delete">X</button></li>`;
+}
+
+function removeElement(e) {
+    if(e.target.classList.contains("delete")) {
+        if(confirm("Are you sure")) {
+            let li = e.target.parentElement;
+            listItems.removeChild(li);
+        }
+    }
+}
+
+function filterElement(e) {
+    let text = e.target.value.toLowerCase();
+    let items = Array.from(listItems.getElementsByTagName("li"));
+    items.forEach(function(item) {
+        var itemName = item.firstChild.textContent.toLowerCase();
+        if (itemName.indexOf(text) != -1) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+        console.log(itemName.indexOf(text));
+    })
+
+    // Array.from(listElements).map(ele => {
+    //     if(ele.textContent.toLowerCase() === text) {
+    //         ele.style.background = "red";
+    //         console.log("im that");
+    //     }
+    // });
+}
+
