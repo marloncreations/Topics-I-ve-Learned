c14008d0fc62329d74938e159fe5e0b4a0a6ffdc
unknown <marlonspr@hotmail.com>
2019-01-17 16:20:00 +1300

    Arrays

---
+++ Array Methods and Properties/index.js
@@ -0,0 +1,25 @@
+// Array Properties
+
+const myArray = [2,3,5,6,2,7,5,8];
+const myString = "Hello";
+const myObject = {
+    name: "Hello", id: 1,
+    name: "Hello", id: 2,
+    name: "Hello", id: 3
+};
+
+// Array.length ------------------------------------------------------------------------------>>>>>>
+
+console.log(myArray.length);
+
+// Array Methods
+
+// Array.isArray() ------------------------------------------------------------------------------>>>>>>
+
+
+
+// console.log(Array.isArray(myArray));
+// console.log(Array.isArray(myString));
+// console.log(Array.isArray(myObject));
+
+// Array.isArray() ------------------------------------------------------------------------------>>>>>>
