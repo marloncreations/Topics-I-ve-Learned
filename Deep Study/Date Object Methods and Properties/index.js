1cde3c3b1fb6ceb6d30be9231327e9313501bf30
Marlon Steve <marlonspr@hotmail.com>
2019-04-07 21:34:13 +1200

    added more of the date object

--- Deep Study/Date Object Methods and Properties/index.js
+++ Deep Study/Date Object Methods and Properties/index.js
@@ -96,3 +96,113 @@ console.log(difference);
 
 // Date.getUTCDate() ---------------------------------------------------------------------------->
 console.log(today.getUTCDate()); // gets day of the month according to universal time
+
+// Date.getUTCDay() ---------------------------------------------------------------------------->
+console.log(today.getUTCDay()); // gets day accoring to universal time
+
+// Date.getUTCFullYear() ---------------------------------------------------------------------------->
+console.log(today.getUTCFullYear()); // gets year accoring to universal time
+
+// Date.getUTCHours() ---------------------------------------------------------------------------->
+console.log(today.getUTCHours()); // gets hours accoring to universal time
+
+// Date.getUTCMilliseconds() ---------------------------------------------------------------------------->
+console.log(today.getUTCMilliseconds()); // gets milliseconds accoring to universal time
+
+// Date.getUTCMinutes() ---------------------------------------------------------------------------->
+console.log(today.getUTCMinutes()); // gets minutes accoring to universal time
+
+// Date.getUTCMonth() ---------------------------------------------------------------------------->
+console.log(today.getUTCMonth()); // gets month accoring to universal time - 0 based index 0=jan 11=dec
+
+// Date.getUTCSeconds() ---------------------------------------------------------------------------->
+console.log(today.getUTCSeconds()); // gets seconds accoring to universal time
+
+// Date.setDate() ---------------------------------------------------------------------------->
+console.log(today.setDate(30)); // sets the day of the Date object relative to the beginning of the currently set month. if a month has 30 days and you input 31 it jumps on the next month and show the number of days extra in this case 1 
+
+console.log(today.getDate());
+console.log(today.getMonth());
+ 
+// Date.prototype.setFullYear() // sets year
+// Date.prototype.setHours() // sets hour and returns number of miliseconds
+// Date.prototype.setMilliseconds() // sets milliseconds for a specified date
+// Date.prototype.setMinutes() // sets minutes for a specified date
+// Date.prototype.setMonth() // sets month for a specified date
+// Date.prototype.setSeconds() // sets seconds for a specified date
+
+// for the set UTCs date, fullyear, hours, milliseconds, minutes, month, seconds
+// ------> works the same as the above methods to set date but for the universal time
+
+// Date.prototype.setUTCDate()
+// Date.prototype.setUTCFullYear()
+// Date.prototype.setUTCHours()
+// Date.prototype.setUTCMilliseconds()
+// Date.prototype.setUTCMinutes()
+// Date.prototype.setUTCMonth()
+// Date.prototype.setUTCSeconds()
+
+// Date.setTime() ----------------------------------------------------------------------------
+// method sets the Date object to the time represented by a number of milliseconds since January 1, 1970, 00:00:00 UTC.
+var event1 = new Date('July 1, 1999');
+var event2 = new Date();
+event2.setTime(event1.getTime());
+
+console.log(event1);
+// expected output: Thu Jul 01 1999 00:00:00 GMT+0200 (CEST)
+console.log(event2);
+
+// Date.toDateString() -------------------------------------------------------------------------
+
+var event = new Date(1993, 6, 28, 14, 39, 7); // transforms this into ---->
+console.log(event.toString()); // -----> To a string that is not that easily understandable
+console.log(event.toDateString()); // -----> To human readable string
+console.log(event.toISOString()); // -----> To simplified extended ISO format
+console.log('-----------------------------------------------------------------------')
+var jsonDate = event.toJSON();
+console.log(jsonDate); // -----> method returns a string representation of the Date object.
+console.log(new Date(jsonDate).toUTCString());
+
+// Date.toLocaleDateString() -------------------------------------------------------------------------
+
+// lets your format the date for different languages through locales with specific options -> returns string
+
+var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+console.log(event.toLocaleDateString('de-DE', options));
+console.log(event.toLocaleDateString('ar-EG', options));
+console.log(event.toLocaleDateString('ko-KR', options));
+
+console.log('---------')
+
+// Date.toLocaleString() -------------------------------------------------------------------------
+
+// lets your format the date for different languages through locales with specific options -> returns string
+
+console.log(event.toLocaleString('en-GB', { timeZone: 'UTC' }));
+console.log(event.toLocaleString('ko-KR', { timeZone: 'UTC' }));
+
+console.log('---------')
+
+// Date.toLocaleTimeString() -------------------------------------------------------------------------
+
+// lets your format the date for different languages through locales with specific options --> for the specific portion of the date (hour, minutes, milliseconds)
+
+console.log(event.toLocaleTimeString('en-US'));
+// expected output: 1:15:30 AM
+console.log(event.toLocaleTimeString('it-IT'));
+// expected output: 01:15:30
+console.log(event.toLocaleTimeString('ar-EG'));
+// expected output: ١٢
+
+// -----------------------
+// Date.prototype.toSource() // non standard returns the source code of the object
+// -----------------------
+
+console.log('---------')
+
+// Date.toString() -------------------------------------------------------------------------
+var event1 = new Date('August 19, 1975 23:15:30');
+console.log(event1.toString()); // to string non understandable
+console.log(event1.toTimeString()); // human readable string form in American English. for the time
+console.log(event1.toUTCString()); // to reaable string using the UTC
+console.log(event1.valueOf()); // returns the date objects primitive type --> which is the number of seconds since 1 January 1970 00:00:00 UTC same as Date.prototype.getTime()
